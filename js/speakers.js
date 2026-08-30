15482502e11e29d5bbbdf58645e0452e81436d7d
Om Rane <41297391+Mercyssh@users.noreply.github.com>
2026-08-30 15:30:48 +0530

    changed speaker order

--- js/speakers.js
+++ js/speakers.js
@@ -10,14 +10,14 @@
 export const SPEAKERS = [
   { name: "Dr. Pavan Mamidi", role: "Director, Ashoka University's Centre for Social and Behaviour Change", summit: "The anatomy of behaviour change", photo: "assets/speakers/Pavan Mamidi.png" },
   { name: "Dr. Sharon Barnhardt", role: "Director - Research, Ashoka University's Centre for Social and Behaviour Change", summit: "The science of behaviour change that lasts", photo: "assets/speakers/Sharon Barnhardt.png" },
-  { name: "Shweta Agarwal", role: "Board Member - CSR, Sterlite Technologies", summit: "How boards analyse moonshot ideas for social impact", photo: "assets/speakers/Shweta Agarwal.png" },
   { name: "Vanshika Goenka", role: "CEO at Christy & Director at Welspun Corp", summit: "How compliance and courage go together", photo: "assets/speakers/Vanshika Goenka.png" },
+  { name: "Dr. Maneesh Mishra", role: "VP, Jindal Steel & Head, Naveen Jindal Foundation", summit: "The ground reality of scale", photo: "assets/speakers/Maneesh Mishra.png" },
   { name: "Aayush Ailawadi", role: "AI & Tech Opinion Leader", summit: "What it takes for AI to amplify impact", photo: "assets/speakers/Aayush Ailawadi.png" },
   { name: "Devavrat Kakade", role: "Executive Director, APAC, JPMorganChase", summit: "Taking bold ideas to a billion people", photo: "assets/speakers/Devavrat Kakade.png" },
   { name: "Geetika Dadlani", role: "Head, Giving Pi & Associate Director, Dasra", summit: "What bold giving looks like from the inside", photo: "assets/speakers/Geetika Dadlani.png" },
+  { name: "Shweta Agarwal", role: "Board Member - CSR, Sterlite Technologies", summit: "How boards analyse moonshot ideas for social impact", photo: "assets/speakers/Shweta Agarwal.png" },
   { name: "Ninad Karpe", role: "Founder & Partner, 100xVC", summit: "The moonshot investment thesis", photo: "assets/speakers/Ninad Karpe.png" },
   { name: "Nirav Khambhati", role: "Partner, The Blended Finance Company", summit: "The case for funding innovation", photo: "assets/speakers/Nirav Khambhati.png" },
-  { name: "Dr. Maneesh Mishra", role: "VP, Jindal Steel & Head, Naveen Jindal Foundation", summit: "The ground reality of scale", photo: "assets/speakers/Maneesh Mishra.png" },
   { name: "Bilal Jaleel", role: "Director, WTF Media", summit: "How narrative can be an impact metric", photo: "assets/speakers/Bilal Jaleel.png" },
 ];
 
